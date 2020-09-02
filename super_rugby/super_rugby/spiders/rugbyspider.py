1645a7c5a18b41cb1c46b3dc1d2d161bf9113d11
Malcolm Smith Fraser <malcolmsfraser@gmail.com>
2020-09-02 23:35:08 +0000

    reoredering the csv output columns"

--- super_rugby/super_rugby/spiders/rugbyspider.py
+++ super_rugby/super_rugby/spiders/rugbyspider.py
@@ -14,13 +14,13 @@ class RugbySpider(scrapy.Spider):
         away_team_score = response.css('tr+ tr td:nth-child(5)').css('::text').extract()
         date = response.css('tr:nth-child(7) td:nth-child(6) , tr:nth-child(8) td:nth-child(6) , tr:nth-child(6) td:nth-child(6) , tr:nth-child(5) td:nth-child(6) , tr:nth-child(4) td:nth-child(6) , tr+ tr td:nth-child(6) div').css('::text').extract()
 
-        for item in zip(home_team,home_team_score,away_team,away_team_score,date):
+        for item in zip(date,home_team,home_team_score,away_team,away_team_score,):
             scrapped_info = {
-                'Match Date' : item[4],
                 'Home Team' : item[0],
                 'Home Team Score' : item[1],
                 'Away Team' : item[2],
                 'Away Team Score' : item[3],
+                'Match Date' : item[4],
                 
             }
             yield scrapped_info
